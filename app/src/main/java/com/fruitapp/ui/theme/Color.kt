a8b3a8c5c7ed3de7314733a3880ccfa787c38649
ambikasoni0802-ai <ambikasoni0802@gmail.com>
2026-09-11 15:29:02 +0530

    Create Color.kt

---
+++ app/src/main/java/com/fruitapp/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.fruitapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val OrangeStart = Color(0xFFFFB347)
+val PinkMid = Color(0xFFFF6F91)
+val PurpleEnd = Color(0xFFC86DD7)
+val GreenBannerStart = Color(0xFF9EE6C4)
+val GreenBannerEnd = Color(0xFFDCEDC1)
+val TextBrown = Color(0xFF5A3E2B)
+val CardBackground = Color(0xF2FFFFFF)
+val BgTop = Color(0xFFFFF9F0)
+val BgMid = Color(0xFFFFF1E4)
+val BgBottom = Color(0xFFFFE7F1)
